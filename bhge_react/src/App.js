6cd3eb71eab6941111801c76af30f6ebeecdf47f
matrice-one <neigeline.rivollat@gmail.com>
2023-06-28 19:47:30 +0200

    adds path and yml for artefact

--- bhge_react/src/App.js
+++ bhge_react/src/App.js
@@ -2,20 +2,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage.js';
-import Header from './components/Header.js';
-// import AboutPage from './components/AboutPage';
-// import NotFoundPage from './components/NotFoundPage';
 
 const App = () => {
   return (
-    <Router>
-      {/* <Header /> */}
+    <Router basename="/bhge">
       <Routes>
       <Route path="/" element={<HomePage />} />
-
-        {/* <Route exact path="/" component={HomePage} /> */}
-        {/* <Route path="/about" component={AboutPage} />
-        <Route component={NotFoundPage} /> */}
       </Routes>
     </Router>
   );
