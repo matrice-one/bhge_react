6ad9ba023bb00e105e16d33102798ac29bbc5c27
matrice-one <91607670+matrice-one@users.noreply.github.com>
2024-06-24 17:43:43 +0200

    feat: sets correct credits

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,12 +1,14 @@
-import { Grid, } from "@mui/material";
+import { Grid, Box, Typography} from "@mui/material";
 import imageGallery1 from "../media/images/gallerie_picture_1.png";
 import imageGallery2 from "../media/images/gallerie_picture_2.png";
 import imageGallery3 from "../media/images/happy_dar.jpeg";
 import imageGallery4 from "../media/images/gallerie_picture_4.png";
 import FullHeightSection from './reusable_components/FullHeightSection';
+import { useTranslation } from "react-i18next";
 
 
 function GallerySection() {
+  const {t} = useTranslation();
   
   return (
     <FullHeightSection bgcolor={"primary.main"}>
@@ -24,6 +26,11 @@ function GallerySection() {
             <img src={imageGallery4} alt="Section" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
           </Grid>
         </Grid>
+        <Box bgcolor={"primary.main"} p={2}>
+        <Typography variant="body1" color={"secondary.main"} align="center">
+        {t("GallerySection.credits")}
+        </Typography>
+      </Box>
       </FullHeightSection>
   );
 }
