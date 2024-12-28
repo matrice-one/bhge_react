5ea91708a13b86bc489e486bd2bc801b41f67f52
matrice-one <91607670+matrice-one@users.noreply.github.com>
2024-12-28 11:12:42 +0100

    chore: extra logos

--- src/components/Supports.js
+++ src/components/Supports.js
@@ -9,6 +9,10 @@ import vbc_etoile_geneve_logo from "../media/logos/VBC_Etoile_Geneve.jpeg";
 import peps_beach_volley_logo from "../media/logos/PEPS_HQ.png";
 import volley_geneve_sud_geneve_logo from "../media/logos/volley_jeunesse_geneve_sud.png";
 import snvb_logo from "../media/logos/SNVB.png";
+import cern_logo from "../media/logos/CERN.png";
+import satigny_logo from "../media/logos/satigny.png";
+import viry_logo from "../media/logos/viry.png";
+import prevessin_logo from "../media/logos/prevessin.png";
 
 // Add more imports as needed
 
@@ -23,82 +27,148 @@ function Supports() {
           <Box>
             <Grid container spacing={4} justifyContent="center">
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://www.ge.ch/">
                 <img
                   src={geneva_canton_logo}
                   alt="logo canton de genève"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                     width: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://www.svrge.ch/">
                 <img
                   src={svrg_logo}
                   alt="swiss volley region genève logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://www.beachcentergeneve.ch/">
                 <img
                   src={franchise_beach_volley_logo}
-                  alt="beach volley franchise logo"
+                  alt="beach volley club vernier logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://vbcgeneve.ch/">
                 <img
                   src={vbc_etoile_geneve_logo}
                   alt="vbc etoile Genève logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://www.pepsvolley.ch/">
                 <img
                   src={peps_beach_volley_logo}
                   alt="peps beach volley logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://volleygenevesud.ch/">
                 <img
                   src={volley_geneve_sud_geneve_logo}
                   alt="volley jeunesse genève sud logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
+                <a href="https://chenoisvolley.ch/adultes/">
                 <img
                   src={snvb_logo}
                   alt="snvb logo"
                   style={{
                     maxWidth: "100%",
-                    maxHeight: "200px",
+                    maxHeight: "150px",
                     height: "auto",
                   }}
                 />
+                </a>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <a href="https://volleyball-club.web.cern.ch/">
+                <img
+                  src={cern_logo}
+                  alt="cern logo"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "150px",
+                    height: "auto",
+                  }}
+                />
+                </a>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <a href="http://www.volleyprevessin.fr/">
+                <img
+                  src={prevessin_logo}
+                  alt="prevessin logo"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "150px",
+                    height: "auto",
+                  }}
+                />
+                </a>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <a href="https://www.satigny-volleyball.ch/">
+                <img
+                  src={satigny_logo}
+                  alt="satigny logo"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "150px",
+                    height: "auto",
+                  }}
+                />
+                </a>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <a href="https://leagues.teamlinkt.com/viryvolleyball/LeClub">
+                <img
+                  src={viry_logo}
+                  alt="viry logo"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "150px",
+                    height: "auto",
+                  }}
+                />
+                </a>
               </Grid>
             </Grid>
           </Box>
